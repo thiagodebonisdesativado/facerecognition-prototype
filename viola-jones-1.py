3bd16a346beb88b5fff8ac41f151450ab3170457
THIAGO DE BONIS <thiagobonis@icloud.com>
2019-10-27 17:25:20 -0300

    Comments in Viola-Jones-1

--- viola-jones-1.py
+++ viola-jones-1.py
@@ -4,6 +4,7 @@ IMPORTANT: CODE DEVELOPED BY THIAGO DE BONIS CARVALHO SAAD SAUD
 '''
 import cv2
 
+# RETURN A PATH OF IMAGES
 def getStaticImage():
     arrStaticImages = ['images\\static\\cats_and_dogs.jpg',
                        'images\\static\\crowd.jpg',
@@ -13,15 +14,21 @@ def getStaticImage():
                        ]
     return arrStaticImages
 
+# CONFIG CLASSIFIER INITIALIZATION
 classifier = cv2.CascadeClassifier('classifiers\\haar\\haarcascade_frontalface_default.xml')
 
+# CONFIG CV2 IMAGE INITIALIZATION
 imageOriginal = cv2.imread(filename=getStaticImage()[1])
 imageConverted = cv2.cvtColor(src=imageOriginal, code=cv2.COLOR_BGR2GRAY)
 
+# CONFIG CLASSIFIER DETECT
 faceDetected = classifier.detectMultiScale(image=imageConverted, scaleFactor=1.1, minNeighbors=1, minSize=(30, 30))
 facesMatrix = faceDetected
 facesAmount = len(faceDetected)
 
+# LOOP THROUGH FACES MATRIX
+# A FACE MATRIX CAN CONTAINER 0..N FACE VECTORS
+# EACH VECTOR WITHIN THE MATRIX IS REFERENCED TO A FACE
 for (x, y, l, a) in facesMatrix:
     pointXY = (x, y)
     pointDrawing = (x + l, y + a)
@@ -43,6 +50,7 @@ for (x, y, l, a) in facesMatrix:
         color=(0, 0, 255)
     )
 
+# CONFIG CV2 IMAGE SHOW AND CLOSED (WAITKEY)
 cv2.imshow("FACES FOUND", imageOriginal)
 cv2.waitKey()
 
